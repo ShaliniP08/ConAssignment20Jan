a49d4ffbfdeb2dc4625ec748a7a8888983dd779b
Shalini Poddar <shalini.poddar@zensar.com>
2021-01-26 17:34:10 +0530

    Add project files.

---
+++ ConAssignment20Jan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using StockMarket;
+
+namespace ConAssignment20Jan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var br = new Broker();
+            var stk = new List<Stock>()
+            {
+                new Stock("ZENSARTECH", 237.10, 50, StockType.Equity),
+                new Stock("TATAMOTORS", 274.40, 70, StockType.MutualFund),
+                new Stock("BURGERKING", 145.05),
+                new Stock("IRCTC", 1451.70, 10, StockType.MutualFund),
+                new Stock("ONGC", 98.50),
+            };
+
+            var ie = stk.GetEnumerator();
+            while(ie.MoveNext())
+            {
+                br.BuyStock(ie.Current);
+            }
+        
+            br.ShowPortfolio();
+
+            Console.ReadLine();
+        }
+    }
+}
